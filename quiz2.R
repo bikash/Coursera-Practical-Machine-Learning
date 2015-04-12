f59df6fee51450aab0b909004a480e1168f37a50
Bikash <er.bikash21@gmail.com>
2015-04-13 01:00:31 +0200

    Q 4

--- quiz2.R
+++ quiz2.R
@@ -65,7 +65,23 @@ ggplot(data = training, aes(x = Superplasticizer)) + geom_histogram() + theme_bw
 
 
 ## Question 4
+library(caret)
+library(AppliedPredictiveModeling)
+set.seed(3433)
+data(AlzheimerDisease)
+adData = data.frame(diagnosis,predictors)
+inTrain = createDataPartition(adData$diagnosis, p = 3/4)[[1]]
+training = adData[ inTrain,]
+testing = adData[-inTrain,]
 
+IL_variables <- grep("^IL", names(training), value = TRUE)
+preProc <- preProcess(training[, IL_variables], method = "pca", thresh = 0.9)
+preProc
+
+### Answer
+## 9
+
+## Question 5
 
 
 
