c4322cdf2e4cfddb0185c0851fcf31c9bd875b28
Bikash <er.bikash21@gmail.com>
2015-11-05 05:20:20 -0500

    quiz2

---
+++ ExploratorydataScience/project2/project.R
@@ -0,0 +1,9 @@
+
+setwd("/Users/bikash/repos/Coursera-Practical-Machine-Learning/ExploratorydataScience/project2")
+# Set seed
+set.seed(123)
+
+
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
